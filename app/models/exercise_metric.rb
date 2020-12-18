f95ea25ca02187fd2cfd5526ffffa8fe3c6040d1
shane <shanecolbyzero8@gmail.com>
2020-12-17 19:36:03 -0600

    add associations to the models

--- app/models/exercise_metric.rb
+++ app/models/exercise_metric.rb
@@ -1,2 +1,4 @@
 class ExerciseMetric < ApplicationRecord
+  belongs_to :user
+  has_many :exercises
 end
--- app/models/selected_exercise.rb
+++ app/models/selected_exercise.rb
@@ -1,2 +1,4 @@
 class SelectedExercise < ApplicationRecord
+  belongs_to :user
+  has_many :exercises
 end
