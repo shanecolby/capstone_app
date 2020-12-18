2459f75cf971c10ed3c10a08af93045ce17667b6
shane <shanecolbyzero8@gmail.com>
2020-12-17 21:23:09 -0600

    add image_url to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,99 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+exercise = Exercise.new(name: "Lat Pulldown", focus: "Back", image_url: "https://allfitnessweb.com/wp-content/uploads/2018/02/wide-grip-lat-pulldown.jpg")
+
+exercise = Exercise.new(name: "Reverse Fly", focus: "Back", image_url: "https://strengthplans.com/wp-content/uploads/2019/03/How-To-Do-A-Dumbbell-Reverse-Flye.png")
+
+exercise = Exercise.new(name: "Horizontal Row", focus: "Back", image_url: "https://static.wixstatic.com/media/d34523_38f568b98a4f4f869a736fa17967304d~mv2.jpg")
+
+exercise = Exercise.new(name: "90degree external shoulder rotation", focus: "Back", image_url: "https://allpickleball.com/wp-content/uploads/2015/01/external1.jpg")
+
+exercise = Exercise.new(name: "Pull up", focus: "Back", image_url: "https://swizec.com/social-cards/Despite-daily-strength-training-Im-not-fit-enough-for-WW2.jpg")
+
+exercise = Exercise.new(name: "Renegade Row", focus: "Back", image_url: "https://www.leanitup.com/wp-content/uploads/2012/10/1001-pushup-position-row-483x300.jpg")
+
+exercise = Exercise.new(name: "Deadlift", focus: "Back", image_url: "https://cdn1.coachmag.co.uk/sites/coachmag/files/styles/insert_main_wide_image/public/2016/05/how_to_deadlift_side_view.jpg?itok=8JVJ4ZqI")
+
+exercise = Exercise.new(name: "Bent over single arm row", focus: "Back", image_url: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/02-single-arm-bent-over-row-1441211036.jpg")
+
+exercise = Exercise.new(name: "Farmers Carry", focus: "Back", image_url: "https://cdn-xi3mbccdkztvoept8hl.netdna-ssl.com/wp-content/uploads/watermarked/Farmers_Walk_Carry.png")
+
+exercise = Exercise.new(name: "Bench Press", focus: "Chest", image_url: "https://workoutlabs.com/wp-content/uploads/watermarked/Barbell_Bench_Press.png")
+
+exercise = Exercise.new(name: "Incline Bench Press", focus: "Chest", image_url: "https://lh3.googleusercontent.com/proxy/YaSUm-SapUxqidhWYV0uYrZja_brqlTaBCcJC8V3DcPGmfMUj6SY4XqUG8mAOCt-JMXiOWlwz61b07V1etWvvmlsXkKrneVF-meU2FLXb9pRFReenhXHnEGip0KwVnnuxES5qyzOCXubX8FKIshiFRn7Ysc0")
+
+exercise = Exercise.new(name: "Decline Bench Press", focus: "Chest", image_url: "https://lh3.googleusercontent.com/proxy/UuNu7c3jquWvf_1gA_vvyizO9-0hal2WTGkH7UFJ4BKsKJTp85_Ue6co0dukyD9gxv1xPwPRvizJzknmtgcT_Fx6oW-UluF-iC3NgRKCwx9_0rsFW1Qm34OLjfD_DdDrl6IkkLxkKa9tIeHMQ-hZ_oBn9joBXQ")
+
+exercise = Exercise.new(name: "Chest fly", focus: "Chest", image_url: "https://global-uploads.webflow.com/5d1d0d3f53ced35a29dbe169/5e2a552a38b3bd766a0167ff_incline-dumbbell-fly-exercise-anabolic-aliens.png")
+
+exercise = Exercise.new(name: "Landmine Press", focus: "Chest", image_url: "https://cdn2.coachmag.co.uk/sites/coachmag/files/styles/insert_main_wide_image/public/2017/12/hammer-jammer.jpg?itok=b-WQpSzc")
+
+exercise = Exercise.new(name: "Dumbbell Pullover", focus: "Chest", image_url: "https://www.burnthefatinnercircle.com/members/images/1868b.jpg?cb=20191017074748")
+
+exercise = Exercise.new(name: "Push Up", focus: "Chest", image_url: "https://static.vecteezy.com/system/resources/previews/000/639/920/non_2x/vector-pushups-black-silhouette.jpg")
+
+exercise = Exercise.new(name: "Wide-grip push up", focus: "Chest", image_url: "https://www.burnthefatinnercircle.com/members/images/1681b.png?cb=20201103100418")
+
+exercise = Exercise.new(name: "Tricep Extension", focus: "Arms", image_url: "https://s3.amazonaws.com/prod.skimble/assets/1530129/image_iphone.jpg")
+
+exercise = Exercise.new(name: "Bicep Curl", focus: "Arms", image_url: "https://i.pinimg.com/originals/67/56/95/67569542aa1125c4c0c3bb70b116b125.png")
+
+exercise = Exercise.new(name: "Reverse Curl", focus: "Arms", image_url: "https://thumbor.thedailymeal.com/TUEHDGKA98eZi6dTQhkHHmvRizI=/870x565/https://www.theactivetimes.com/sites/default/files/uploads/7/7-shutterstock_418630528.jpg")
+
+exercise = Exercise.new(name: "Arnold Press", focus: "Arms", image_url: "https://i.pinimg.com/originals/0d/24/c8/0d24c8b5af79afde4be5b7325d17d1af.jpg")
+
+exercise = Exercise.new(name: "Overhead Press", focus: "Arms", image_url: "https://cdn2.coachmag.co.uk/sites/coachmag/files/2016/07/4-1-overhead-press.jpg")
+
+exercise = Exercise.new(name: "Shoulder Abduction", focus: "Arms", image_url: "https://cdn.shopify.com/s/files/1/1075/8446/files/exercise-32.jpg?0")
+
+exercise = Exercise.new(name: "Diamond Push Up", focus: "Arms", image_url: "https://images.squarespace-cdn.com/content/v1/55e406fbe4b0b03c5e7543ae/1588273637239-QAA997U1TA0CGDYOJNUE/ke17ZwdGBToddI8pDm48kHJOPaoZlb1VYl2j0E1XklYUqsxRUqqbr1mOJYKfIPR7LoDQ9mXPOjoJoqy81S2I8N_N4V1vUb5AoIIIbLZhVYxCRW4BPu10St3TBAUQYVKcXwG1eq3VFHR_iwF-oXd3xYsNSfO8xxLoqpGCyoFaKVKYdPwkiW3TefeGaTBBhcRn/Diamond+Push+Ups")
+
+exercise = Exercise.new(name: "Curl to Overhead Press", focus: "Arms", image_url: "https://i.pinimg.com/originals/42/aa/4b/42aa4b545a6eb79f93406ad73da7e65f.png")
+
+exercise = Exercise.new(name: "Barbell Squat", focus: "Legs", image_url: "https://www.researchgate.net/profile/Najam_Syed2/publication/316914736/figure/fig9/AS:506783883698181@1497838101921/Barbell-back-squat-reproduced-from-61.png")
+
+exercise = Exercise.new(name: "Reverse Lunge", focus: "Legs", image_url: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/workouts/2016/05/take-it-with-you-composites4-1462306310.jpg?resize=480:*")
+
+exercise = Exercise.new(name: "Goblet Sumo Squat", focus: "Legs", image_url: "https://hiitacademy.com/wp-content/uploads/2015/02/goblet-squat-500.jpg")
+
+exercise = Exercise.new(name: "Crossover Squat", focus: "Legs", image_url: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/1076/bodyshop-9-785-1513042322.jpg?resize=480:*")
+
+exercise = Exercise.new(name: "Squat Jump", focus: "Legs", image_url: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/squat-jump-1583856594.jpg?resize=480:*")
+
+exercise = Exercise.new(name: "Glute Bridge", focus: "Legs", image_url: "https://cdn.prod.openfit.com/uploads/2016/09/Glute-Bridge-A.B.jpg")
+
+exercise = Exercise.new(name: "Box Jump", focus: "Legs", image_url: "https://www.gymguider.com/wp-content/uploads/2017/10/box-jump.jpg")
+
+exercise = Exercise.new(name: "Burpee", focus: "Legs", image_url: "https://www.sammyrusso.com/wp-content/uploads/2017/06/burpee-progression.jpg")
+
+
+exercise = Exercise.new(name: "Split Squat", focus: "Legs", image_url: "https://cdn1.coachmag.co.uk/sites/coachmag/files/styles/16x9_480/public/2017/11/dumbbell-split-squat.jpg?itok=0dlpUdI5&timestamp=1510144118")
+
+exercise = Exercise.new(name: "Romanian Deadlift", focus: "Legs", image_url: "https://www.t-nation.com/system/publishing/articles/10006580/original/The-Best-Way-to-Do-Romanian-Deadlifts.jpg?1533839870")
+
+exercise = Exercise.new(name: "Goblet Squat", focus: "Legs", image_url: "https://images.squarespace-cdn.com/content/v1/55e406fbe4b0b03c5e7543ae/1496703462807-CNFGV2YRMX4GOJ52RGVZ/ke17ZwdGBToddI8pDm48kDnii2wljczCzFlPdaJfiT1Zw-zPPgdn4jUwVcJE1ZvWEtT5uBSRWt4vQZAgTJucoTqqXjS3CfNDSuuf31e0tVHPyMdB7LvJYBzQUySDIawKGkxz6UK2zf29cveIJtEHS2bSd6kfRtgWHgNMDgGnmDY/Goblet+Squats")
+
+exercise = Exercise.new(name: "Lateral Lunge", focus: "Legs", image_url: "https://moriartypt.com/wp-content/uploads/2017/07/Lateral-Lunges.jpg")
+
+exercise = Exercise.new(name: "Single Leg Deadlift", focus: "Legs", image_url: "https://cdn.prod.openfit.com/uploads/2019/02/15143825/single-arm-single-leg-Romanian-deadlift-proprioception.jpg")
+
+exercise = Exercise.new(name: "Plank", focus: "Core", image_url: "https://www.ekhartyoga.com/media/images/articles/content/Forearm-plank-pose-Esther-Ekhart.jpg")
+
+exercise = Exercise.new(name: "Mountain Climber", focus: "Core", image_url: "https://gogoodguru.com/wp-content/uploads/2018/05/gogoodguru.com-mountain-climbers.png")
+
+exercise = Exercise.new(name: "Deadbug", focus: "Core", image_url: "https://www.squareboxfitness.com/wp-content/uploads/2015/03/dead-bug.jpeg")
+
+exercise = Exercise.new(name: "Leg Clap", focus: "Core", image_url: "https://s3.amazonaws.com/prod.skimble/assets/1827824/image_iphone.jpg")
+
+exercise = Exercise.new(name: "Russian Twist", focus: "Core", image_url: "https://maltapersonaltraining.com/wp-content/uploads/2014/06/russian-twist-.jpg")
+
+exercise = Exercise.new(name: "Inch Worm", focus: "Core", image_url: "https://i.pinimg.com/originals/ed/2e/83/ed2e83c6fad3a9fd6573f830e09dbc95.jpg")
+
+exercise = Exercise.new(name: "High Plank Shoulder Tap", focus: "Core", image_url: "add_later")
+
+exercise = Exercise.new(name: "Leg Raise", focus: "Core", image_url: "add_later")
+
+exercise = Exercise.new(name: "Static Bear Hold", focus: "Core", image_url: "add_later")
+
+exercise = Exercise.new(name: "Butterfly Sit Up", focus: "Core", image_url: "add_later")
