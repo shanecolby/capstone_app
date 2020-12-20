8ce2432ed6659b93a51856edd2c958f2dc503197
shane <shanecolbyzero8@gmail.com>
2020-12-20 16:33:30 -0600

    make show action for particular exercise

--- app/controllers/api/exercises_controller.rb
+++ app/controllers/api/exercises_controller.rb
@@ -1,2 +1,22 @@
 class Api::ExercisesController < ApplicationController
+  def index
+    @exercises = Exercise.all
+    render "index.json.jb"
+
+  end
+
+  def show
+    @exercise = Exercise.find_by(id: params[:id])
+    render "show.json.jb"
+  end
+
+  # def create
+  # end
+
+  # def update
+  # end
+
+  # def destroy
+  # end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   # get "/photos" => "photos#index"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+    get "/exercises" => "exercises#index"
+    get "/exercises/:id" => "exercises#show"
+  end
 end
