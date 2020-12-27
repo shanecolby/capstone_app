283f95e0f6e875abb4bd3ca10eee0646f0508fc5
shane <shanecolbyzero8@gmail.com>
2020-12-27 16:37:47 -0600

    add show and index views to selected exercises

--- app/controllers/api/selected_exercises_controller.rb
+++ app/controllers/api/selected_exercises_controller.rb
@@ -1,23 +1,26 @@
 class Api::SelectedExercisesController < ApplicationController
-  before_action: :authenticate_user
-  
+  # before_action: :authenticate_user
+
   def index
     if current_user
       @selected_exercises = current_user.selected_exercises.where(status: "added")
       render "index.json.jb"
     else
       render json: "Please sing into your account"
+    end
   end
+
 
 
   def create
     @selected_exercise = SelectedExercise.create(
-      exercise_id: ,
-      user_id: ,
-      status: ,
-      workout_id: 
+      exercise_id: 1,
+      user_id: 1,
+      status: "",
+      workout_id: 1,
+      exercise_name: "name"
     )
-
     render "show.json.jb"
   end
+
 end
