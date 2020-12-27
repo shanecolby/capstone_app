f18a305f199dd312eca1b92d1eb5487912e4bb2f
shane <shanecolbyzero8@gmail.com>
2020-12-27 15:53:21 -0600

    add workouts controller

---
+++ app/controllers/api/workouts_controller.rb
@@ -0,0 +1,25 @@
+class Api::WorkoutsController < ApplicationController
+  def index
+    @workouts = current_user.workouts
+    render "index.json.jb"
+  end
+
+  def create
+    selected_exercises = current_user.selected_exercises.where(status: "selected")
+
+    @workout = Workout.new(
+      user_id: current_user.id,
+      workout_name: params[:workout_name]
+    )
+
+    @workout.save
+
+    selected_exercises.each do |se|
+      se.update(status: "added to workout", workout_id: @workout_id)
+  end
+
+  def show
+    @workout = current_user.workouts.find_by(id: params[:id])
+    render "show.json.jb"
+  end
+end
