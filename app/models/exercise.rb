c046b1b11f90eb23c7c29094a1baf7e680034678
shane <shanecolbyzero8@gmail.com>
2021-01-03 14:56:22 -0600

    working insomnia parts

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,5 +1,4 @@
 class Exercise < ApplicationRecord
-  belongs_to :user
   has_many :workouts
   has_many :selected_exercises
 end
--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,5 +1,4 @@
 class Workout < ApplicationRecord
   belongs_to :user
-  belongs_to :exercise
   has_many :selected_exercises
 end
