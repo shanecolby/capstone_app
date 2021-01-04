14f7519cbb695a8bb4e5c31d15a4fd54a9d8321c
shane <shanecolbyzero8@gmail.com>
2021-01-04 07:13:38 -0600

    selected exercise cart will show current user exercise selections

--- app/controllers/api/selected_exercises_controller.rb
+++ app/controllers/api/selected_exercises_controller.rb
@@ -2,7 +2,7 @@ class Api::SelectedExercisesController < ApplicationController
   # before_action: :authenticate_user
 
   def index
-    @selected_exercises = SelectedExercise.all
+    @selected_exercises = current_user.selected_exercises 
     render 'index.json.jb'
   end
 
